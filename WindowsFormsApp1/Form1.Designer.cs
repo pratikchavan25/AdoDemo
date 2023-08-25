7494c39b2e46806a8b429885a87a2c3c5adb4049
Pratik <Pratik@DESKTOP-9189TTI>
2023-08-25 10:52:33 +0530

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbluser = new System.Windows.Forms.Label();
+            this.lblpass = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.lblusername = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbluser
+            // 
+            this.lbluser.AutoSize = true;
+            this.lbluser.Location = new System.Drawing.Point(82, 67);
+            this.lbluser.Name = "lbluser";
+            this.lbluser.Size = new System.Drawing.Size(10, 16);
+            this.lbluser.TabIndex = 0;
+            this.lbluser.Text = " ";
+            this.lbluser.Click += new System.EventHandler(this.txtusername_Click);
+            // 
+            // lblpass
+            // 
+            this.lblpass.AutoSize = true;
+            this.lblpass.Location = new System.Drawing.Point(82, 161);
+            this.lblpass.Name = "lblpass";
+            this.lblpass.Size = new System.Drawing.Size(67, 16);
+            this.lblpass.TabIndex = 1;
+            this.lblpass.Text = "Password";
+            this.lblpass.Click += new System.EventHandler(this.txtpassword_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(279, 64);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(280, 22);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(279, 161);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(280, 22);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(279, 256);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "Login";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(429, 256);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "Cancel";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // lblusername
+            // 
+            this.lblusername.AutoSize = true;
+            this.lblusername.Location = new System.Drawing.Point(82, 70);
+            this.lblusername.Name = "lblusername";
+            this.lblusername.Size = new System.Drawing.Size(70, 16);
+            this.lblusername.TabIndex = 6;
+            this.lblusername.Text = "Username";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 355);
+            this.Controls.Add(this.lblusername);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblpass);
+            this.Controls.Add(this.lbluser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbluser;
+        private System.Windows.Forms.Label lblpass;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label lblusername;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,64 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+//namespace WindowsFormsApp1
+//{
+//    public partial class Form1 : Form
+//    {
+//        public Form1()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void Form1_Load(object sender, EventArgs e)
+//        {
+
+//        }
+
+//        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+//        {
+
+//        }
+
+//        private void label3_Click(object sender, EventArgs e)
+//        {
+            
+//        }
+
+//        private void txtusername_Click(object sender, EventArgs e)
+//        {
+            
+//        }
+
+//        private void txtpassword_Click(object sender, EventArgs e)
+//        {
+           
+//        }
+
+//        private void btn1_Click(object sender, EventArgs e)
+//        {
+//            if (txtUserName.Text == "admin" && txtPassword.Text == "admin007")
+//            {
+//                MessageBox.Show("Login Success");
+//            }
+//            else
+//            {
+//                MessageBox.Show("Login Failed");
+//            }
+//        }
+
+//        private void btn2_Click(object sender, EventArgs e)
+//        {
+//            txtPassword.Clear();
+//            txtUserName.Clear();
+
+//        }
+//    }
+//}
